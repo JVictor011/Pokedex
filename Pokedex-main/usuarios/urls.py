1a0a9269f5eeef929a7dd8a0d74d4de086fb2146
João Victor <91631521+JVictor011@users.noreply.github.com>
2023-07-12 10:08:44 -0300

    pokedex

---
+++ Pokedex-main/usuarios/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('login/', views.logar, name="login"),
+    path('home/', views.home, name="home"),
+    path('addTreinador', views.addTreinador, name="addTreinador"),
+    path('addItens/', views.addItens, name="addItens"),
+    path('delete-pokemon/<int:pokemon_id>/', views.deletePokemon, name='deletePokemon'),
+]
